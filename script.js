94a688d40a0cd9a7aa2d8b14b336413afc12d9d4
Bilalzafar0020 <bilalafridi0020@gmail.com>
2023-04-11 19:54:04 +0500

    Working on it

---
+++ script.js
@@ -0,0 +1,46 @@
+function bilal(){
+    document.getElementById(`blockNone`).style.display="block";
+}
+
+function main() {
+    let value1 = document.querySelector(`#mainMonths`).value
+  //   let currentFullDate = new Date()
+  //   let currentMonth = currentFullDate.getMonth()
+  //  let NewMonth=""
+  // NewMonth=Number(NewMonth)
+  //   if (value1 === 'January') {
+
+  //       NewMonth = 0
+  //   }
+  //   let months = NewMonth - currentMonth
+
+value1=Number(value1)
+    let returnValue = "";
+      if (value1 === January) {
+      returnValue = "3";
+    }
+    document.querySelector(`#result1`).innerHTML = months;
+  }
+ 
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    //  when click on button (result section should display block)
+        
+           //  for timer how many second later should (result section display none)
+       
+// setTimeout(function()  {
+//     document.getElementById('blockNone').style.display = 'none';
+// }, 3000); // 3000 milliseconds = 3 seconds
