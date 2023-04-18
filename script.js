aa12df54c506ba842787a8c4c7b56bf0c189d7bd
Bilalzafar0020 <bilalafridi0020@gmail.com>
2023-04-18 20:07:23 +0500

    UPcoming birthday calculator

--- script.js
+++ script.js
@@ -62,18 +62,17 @@ let currentMonth=currentTime.getMonth();
 let month=userMonth - (currentMonth+1);
 
 
-
+//   for (-) values
 if(month< 0){
       month=month+12
   }else{
       month=month
   };
+
+
+  //  for months and month in answer   
   let addingMonths= "Months";
 
-
-
-//   addingMonths=Number(addingMonths)
-  
   if(month===1 || month===0){
       addingMonths="Month";}
   else{
@@ -82,10 +81,52 @@ if(month< 0){
   
 
 
-  let combine1= `${month} ${addingMonths}`
 
 
 
+
+//   now  for date  (to print date for the remaing of birthday) 
+
+ let  userDate=document.querySelector(`#mainDates`).value 
+userDate = Number(userDate)
+
+
+let currentDate = currentTime.getDate(); 
+currentDate=Number(currentDate)
+
+
+
+let date = userDate - currentDate;
+
+
+    ///  now for (-) dates 
+
+    if(date < 0 ){
+      date= date + 30 ;
+      month =   month - 1;
+    }else{
+      date = date
+    };
+
+
+//  for date and dates in result
+let addingDate = "Days";
+
+if(date ===1 || date === 0){
+  addingDate = "Day"
+}else{
+  addingDate = addingDate
+};
+
+
+//   combine answerr of month  and addingmonths(why we are taking here and not above with it's 
+//    sector   because the month in (-) of date will not reduce the month when date will become -)
+
+
+    let combine1= `${month} ${addingMonths}`;
+
+
+let combine2 = `${date} ${addingDate}`;
 
 
 
@@ -96,9 +137,9 @@ if(month< 0){
     
   document.querySelector(`#blockNone`).style.display="block";
 
+document.querySelector(`#result2`).innerHTML=combine2 ;
 
-
-   }
+   };
 
 
 
